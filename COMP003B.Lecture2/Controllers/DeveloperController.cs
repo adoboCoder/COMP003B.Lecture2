5b2463722aca00f838462a1eb09f251fa6a1a484
adobocoder <jcruz@adobo.dev>
2023-03-27 19:39:20 -0700

    Add project files.

---
+++ COMP003B.Lecture2/Controllers/DeveloperController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Lecture2.Controllers
+{
+    public class DeveloperController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Hobbies()
+        {
+            return View();
+        }
+    }
+}
